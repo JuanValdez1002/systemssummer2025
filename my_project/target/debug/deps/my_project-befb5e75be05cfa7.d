acc0d282a7d471ca2b26a41df31e0d9eddeb8b8d
Juan Miguel Valdez <147435601+JuanValdez1002@users.noreply.github.com>
2025-07-18 21:14:37 +0000

    My first project

---
+++ my_project/target/debug/deps/my_project-befb5e75be05cfa7.d
@@ -0,0 +1,5 @@
+/workspaces/systemssummer2025/my_project/target/debug/deps/my_project-befb5e75be05cfa7.d: src/main.rs
+
+/workspaces/systemssummer2025/my_project/target/debug/deps/libmy_project-befb5e75be05cfa7.rmeta: src/main.rs
+
+src/main.rs:
