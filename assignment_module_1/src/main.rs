f08976e470f9ed05e1f973325d52baf8595d8f1b
Juan Miguel Valdez <147435601+JuanValdez1002@users.noreply.github.com>
2025-08-02 23:04:15 +0000

    Module_1 Assignment_1complete

--- assignment_module_1/src/main.rs
+++ assignment_module_1/src/main.rs
@@ -1,3 +1,21 @@
+const FREEZING_POINT_F: f64 = 32.0;
+
+fn fahrenheit_to_celsius(f: f64) -> f64 {
+    (f - 32.0) * 5.0 / 9.0
+}
+
+fn celsius_to_fahrenheit(c: f64) -> f64 {
+    c * 9.0 / 5.0 + 32.0
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut temp_f = FREEZING_POINT_F;
+    let temp_c = fahrenheit_to_celsius(temp_f);
+    println!("{:.1}°F is {:.1}°C", temp_f, temp_c);
+
+    for i in 1..=5 {
+        temp_f += 1.0;
+        let temp_c = fahrenheit_to_celsius(temp_f);
+        println!("{:.1}°F is {:.1}°C", temp_f, temp_c);
+    }
 }
